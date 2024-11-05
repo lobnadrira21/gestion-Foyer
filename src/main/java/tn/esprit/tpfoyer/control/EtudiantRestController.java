3485130f8b6417dd77aaeedb56e3927c3c2eda2a
RzemMoahemd <mohamed.rzem@esprit.tn>
2024-11-05 18:25:01 +0100

    modif

--- src/main/java/tn/esprit/tpfoyer/control/EtudiantRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/EtudiantRestController.java
@@ -19,6 +19,7 @@ public class EtudiantRestController {
     @GetMapping("/retrieve-all-etudiants")
     public List<Etudiant> getEtudiants() {
         List<Etudiant> listEtudiants = etudiantService.retrieveAllEtudiants();
+        System.out.println(listEtudiants.get(0));
         return listEtudiants;
     }
 
