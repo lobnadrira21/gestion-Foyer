bcde369d4431f0eb82e25e98296c2f17edf25f41
emna <emna.khlifaoui@esprit.tn>
2024-11-07 11:09:40 +0100

    junit Test modifications

--- src/test/java/BlocServiceImplTest.java
+++ src/test/java/BlocServiceImplTest.java
@@ -54,4 +54,65 @@ public class BlocServiceImplTest {
         verify(blocRepository, times(1)).save(bloc);
     }
 
+    @Test
+    public void testModifyBloc() {
+        // Arrange
+        Bloc bloc = new Bloc();
+
+        when(blocRepository.save(bloc)).thenReturn(bloc);
+
+        // Act
+        Bloc result = blocService.modifyBloc(bloc);
+
+        // Assert
+        assertNotNull(result);
+        verify(blocRepository, times(1)).save(bloc);
+    }
+
+    @Test
+    public void testRemoveBloc() {
+        // Arrange
+        Long blocId = 1L;
+
+        // Act
+        blocService.removeBloc(blocId);
+
+        // Assert
+        verify(blocRepository, times(1)).deleteById(blocId);
+    }
+    @Test
+    public void testTrouverBlocsSansFoyer() {
+        // Arrange
+        Bloc bloc1 = new Bloc();
+        Bloc bloc2 = new Bloc();
+
+        List<Bloc> blocsSansFoyer = Arrays.asList(bloc1, bloc2);
+        when(blocRepository.findAllByFoyerIsNull()).thenReturn(blocsSansFoyer);
+
+        // Act
+        List<Bloc> result = blocService.trouverBlocsSansFoyer();
+
+        // Assert
+        assertEquals(2, result.size());
+        verify(blocRepository, times(1)).findAllByFoyerIsNull();
+    }
+
+    @Test
+    public void testTrouverBlocsParNomEtCap() {
+        // Arrange
+        Bloc bloc = new Bloc();
+        String nom = "Bloc A";
+        long capacite = 100;
+
+        List<Bloc> blocs = List.of(bloc);
+        when(blocRepository.findAllByNomBlocAndCapaciteBloc(nom, capacite)).thenReturn(blocs);
+
+        // Act
+        List<Bloc> result = blocService.trouverBlocsParNomEtCap(nom, capacite);
+
+        // Assert
+        assertEquals(1, result.size());
+        verify(blocRepository, times(1)).findAllByNomBlocAndCapaciteBloc(nom, capacite);
+    }
+
 }
